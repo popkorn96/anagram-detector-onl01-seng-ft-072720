e4693119b97ed4d81c62d8b7a9a5cd4801881e3f
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 22:15:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end
   
   def match(array_of_words)
-   array_of_words.select do |element|
+   array_of_words.detect do |element|
      (@word.split("").sort) == (element.split("").sort)
    end
  end
